c24c50410c06aa976ac44902f5c4ec1f3c25ba06
gorbunovakris4 <51321340+gorbunovakris4@users.noreply.github.com>
2023-05-22 20:14:52 +0300

    Create question_patterns.py

---
+++ src/question_patterns.py
@@ -0,0 +1,34 @@
+active = [
+    [("future hopes", 1), ("future hopes", 2), ("confidence", 0)],
+    [("cans", 0), ("cans", 2)],
+    [("confidence", 0), ("cans", 0), ("cans", 2)],
+    [("future hopes", 1), ("future hopes", 2)],
+    [],
+    [],
+    [],
+    [("confidence", 0), ("cans", 0), ("cans", 2)],
+    [("future hopes", 1), ("future hopes", 2), ("confidence", 0), ("cans", 0), ("cans", 2)],
+    [("confidence", 0), ("cans", 0), ("cans", 2)]
+    # ("future hopes", 1), ("future hopes", 2), ("confidence", 0), ("cans", 0), ("cans", 2)
+]
+
+reflex = [
+    [("future hopes", 0), ("past regrets", 0), ("confidence", 1), ("confidence", 2), ("confidence", 3),
+     ("questions", 0), ("questions", 1)],
+    [("confidence", 1), ("confidence", 2), ("confidence", 3), ("questions", 0), ("questions", 1), ("cans", 1),
+     ("confidence", 4)],
+    [("confidence", 1), ("confidence", 2), ("confidence", 3), ("questions", 0), ("questions", 1), ("cans", 1),
+     ("confidence", 4)],
+    [("future hopes", 0), ("past regrets", 0), ("questions", 0), ("questions", 1)],
+    [],
+    [],
+    [],
+    [("confidence", 1), ("confidence", 2), ("confidence", 3), ("questions", 0),
+     ("questions", 1), ("confidence", 4)],
+    [("confidence", 1), ("confidence", 2), ("confidence", 3), ("questions", 0),
+     ("questions", 1), ("confidence", 4)],
+    [("confidence", 1), ("confidence", 2), ("confidence", 3), ("confidence", 4), ("questions", 0),
+     ("questions", 1)],
+    # ("future hopes", 0), ("past regrets", 0), ("confidence", 1), ("confidence", 2), ("confidence", 3), ("questions", 0),
+    # ("questions", 1)
+]
